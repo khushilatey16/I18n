1bdf42cb25d86772bc0797f2fd0f45c291fb14b8
khushilatey16 <khushilatey16@gmail.com>
2024-03-25 20:55:07 +0530

    utils added

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import "./Home.css"
+import React from 'react';
+import "./Home.css";
+import I18n from './../../utils/I18n';
 
 function Home() {
+    const usersCount=100;
+
+   
   return (
-    <div>Home</div>
+    <div>
+        
+        <h1>{I18n("welcomeMessage")}</h1>
+        <p>{I18n("normalMessage")}</p>
+        <h1>{I18n("greetMessage")} </h1>
+        <h2>{I18n("endMessage")} </h2>
+
+        <select
+        defaultValue={localStorage.getItem("lang")}
+        onChange={(e)=>{
+            localStorage.setItem("lang",e.target.value);
+            window.location.reload();
+        }}>
+            <option value="mr">marathi</option>
+            <option value="en">english</option>
+            <option value="hi">hindi</option>
+        </select>
+
+
+        <p>
+          {I18n ("usersStatMessage","<studentCount>" , usersCount)} 
+        </p>
+    </div>
   )
 }
 
